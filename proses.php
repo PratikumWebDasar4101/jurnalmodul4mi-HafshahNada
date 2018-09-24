f25ed3ed3e2d21c78cbe8ac4406dc431da1f33be
HafshahNada <42725368+HafshahNada@users.noreply.github.com>
2018-09-24 11:00:04 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,3 +1,17 @@
 <?php
-
-?>
+$Hobi = $_POST['Hobi'];
+	
+	foreach ($Hobi as $key => $value) {
+		
+	}
+	{
+	print_r($_POST['Hobi']);
+}
+	$file = $_FILES['file'];
+	$tmp_name = $_FILES['tmp_name'];
+	$uplaod_dir ="uplaod/";
+	move_uploaded_file($tmp_name,$uplaod_dir,.$namafile);
+	echo "uploaded.<br>";
+	?>
+	
+	<img src="uplaod/<?php echo $namafile; ?>">
